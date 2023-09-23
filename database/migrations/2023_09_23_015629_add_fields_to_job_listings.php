5e41fb123253739b73584a98a0772eacfb5dfa2a
Nanguti <g.nanguti@gmail.com>
2023-09-23 05:31:22 +0300

    job listing

---
+++ database/migrations/2023_09_23_015629_add_fields_to_job_listings.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            $table->string('date_published');
+            $table->text('job_key_info');
+            $table->longText('job_details');
+            $table->text('application_method');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('job_listings', function (Blueprint $table) {
+            //
+        });
+    }
+};
