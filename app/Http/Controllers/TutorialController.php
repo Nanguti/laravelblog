63c03e5ad6c23935c58ab71c6648a45e5d5b4ae9
Nanguti <g.nanguti@gmail.com>
2023-11-10 12:06:48 +0300

    Feature tutorials

--- app/Http/Controllers/TutorialController.php
+++ app/Http/Controllers/TutorialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Tutorial;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,9 @@ class TutorialController extends Controller
     public function index()
     {
         $tutorials = Tutorial::paginate(20);
+        $categories = Category::all();
         return response()->json([
+            'categories' => $categories,
             'tutorials'=>$tutorials
         ]);
     }
